909c0e4119f4c4897fbc9d92e29e80f4c147ed78
Pierre <sim.pierre91@gmail.com>
2019-06-16 19:03:16 +0200

    add blacklist routes

--- src/main/scala/rest/UserRoutes.scala
+++ src/main/scala/rest/UserRoutes.scala
@@ -29,13 +29,13 @@ class UserRoutes(modules: Configuration with PersistenceModule with DbModule wit
     get {
       onComplete(modules.usersDal.findAll()) {
         case Success(users) => complete(users)
-        case Failure(ex) => complete(InternalServerError, s"An error occurred: ${ex.getMessage}")
+        case Failure(ex) => complete(InternalServerError, s"{ error: 'An error occurred: ${ex.getMessage}' }")
       }
     }
   }
 
   @Path("/{id}")
-  @ApiOperation(value = "Return User", notes = "", nickname = "", httpMethod = "GET")
+  @ApiOperation(value = "Return User", notes = "", nickname = "", httpMethod = "GET", produces = "application/json")
   @ApiImplicitParams(Array(
     new ApiImplicitParam(name = "id", value = "User Id", required = false, dataType = "int", paramType = "path")
   ))
@@ -47,20 +47,19 @@ class UserRoutes(modules: Configuration with PersistenceModule with DbModule wit
   ))
   def userGetRoute = path("users" / IntNumber) { (id) =>
     get {
-      validate(id > 0, "{ error: 'The supplier id should be greater than zero !' }") {
-
+      validate(id > 0, s"{ error: 'The supplier id should be greater than zero !' }") {
         onComplete(modules.usersDal.findOne(id)) {
           case Success(userOpt) => userOpt match {
             case Some(user) => complete(user)
-            case None => complete(NotFound, s"The user doesn't exist")
+            case None => complete(NotFound, s"{ error: 'The user ${id} doesn't exist !' }")
           }
-          case Failure(ex) => complete(InternalServerError, s"An error occurred: ${ex.getMessage}")
+          case Failure(ex) => complete(InternalServerError, s"{ error: 'An error occurred: ${ex.getMessage}' }")
         }
       }
     }
   }
 
-  @ApiOperation(value = "Add User", notes = "", nickname = "", httpMethod = "POST", produces = "text/plain")
+  @ApiOperation(value = "Add User", notes = "", nickname = "", httpMethod = "POST", produces = "application/json")
   @ApiImplicitParams(Array(
     new ApiImplicitParam(name = "body", value = "User Object", required = true,
       dataType = "persistence.entities.SimpleUser", paramType = "body")
@@ -74,14 +73,14 @@ class UserRoutes(modules: Configuration with PersistenceModule with DbModule wit
     post {
       entity(as[SimpleUser]) { userToInsert =>
         onComplete(modules.usersDal.save(User(None, Option(userToInsert.pseudo), Option(userToInsert.sub), Option(userToInsert.blacklist)))) {
-          case Success(_) => complete(Created)
-          case Failure(ex) => complete(InternalServerError, s"An error occurred: ${ex.getMessage}")
+          case Success(user) => complete(user)
+          case Failure(ex) => complete(InternalServerError, s"{ error: 'An error occurred: ${ex.getMessage}' }")
         }
       }
     }
   }
 
-  @ApiOperation(value = "Update User", notes = "", nickname = "", httpMethod = "PATCH", produces = "text/plain")
+  @ApiOperation(value = "Update User", notes = "", nickname = "", httpMethod = "PATCH", produces = "application/json")
   @ApiImplicitParams(Array(
     new ApiImplicitParam(name = "body", value = "User Object", required = true,
       dataType = "persistence.entities.User", paramType = "body")
@@ -91,12 +90,12 @@ class UserRoutes(modules: Configuration with PersistenceModule with DbModule wit
     new ApiResponse(code = 400, message = "Bad Request"),
     new ApiResponse(code = 201, message = "Entity Updated")
   ))
-  def userUpdateRoute = path("users") {
+  def userPatchRoute = path("users") {
     patch {
       entity(as[User]) { userToUpdate =>
         onComplete(modules.usersDal.update(userToUpdate)) {
-          case Success(_) => complete(OK)
-          case Failure(ex) => complete(InternalServerError, s"An error occurred: ${ex.getMessage}")
+          case Success(user) => complete(user)
+          case Failure(ex) => complete(InternalServerError, s"{ error: 'An error occurred: ${ex.getMessage}' }")
         }
       }
     }
@@ -117,13 +116,58 @@ class UserRoutes(modules: Configuration with PersistenceModule with DbModule wit
       entity(as[UserId]) { userIdToDelete =>
         onComplete(modules.usersDal.delete(User(Option(userIdToDelete.id), None, None, None))) {
           case Success(_) => complete(OK)
-          case Failure(ex) => complete(InternalServerError, s"An error occurred: ${ex.getMessage}")
+          case Failure(ex) => complete(InternalServerError, s"{ error: 'An error occurred: ${ex.getMessage}' }")
         }
       }
     }
   }
 
-  val routes: Route = usersGetRoute ~ userGetRoute ~ userPostRoute ~ userUpdateRoute ~ userDeleteRoute
+  @Path("/blacklist")
+  @ApiOperation(value = "Return all blacklisted users", notes = "", nickname = "", httpMethod = "GET", produces = "application/json")
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Return blacklisted users", response = classOf[User]),
+    new ApiResponse(code = 500, message = "Internal server error")
+  ))
+  def usersGetBlacklistedRoute = path("users" / "blacklist") {
+    get {
+      onComplete(modules.usersDal.findAll()) {
+        case Success(users) => complete(users.filter(user => user.blacklist.get > 0))
+        case Failure(ex) => complete(InternalServerError, s"{ error: 'An error occurred: ${ex.getMessage}' }")
+      }
+    }
+  }
+
+  @Path("/blacklist")
+  @ApiOperation(value = "Blacklist user", notes = "", nickname = "", httpMethod = "PATCH", produces = "application/json")
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "body", value = "User id Object", required = true,
+      dataType = "persistence.entities.UserId", paramType = "body")
+  ))
+  @ApiResponses(Array(
+    new ApiResponse(code = 201, message = "Blacklist users"),
+    new ApiResponse(code = 500, message = "Internal server error")
+  ))
+  def usersBlacklistRoute = path("users" / "blacklist") {
+    patch {
+      entity(as[UserId]) { userToUpdate =>
+        onComplete(modules.usersDal.findOne(userToUpdate.id)) {
+          case Success(userOpt) => userOpt match {
+            case Some(user) => {
+              onComplete(modules.usersDal.update(User(user.id, user.pseudo, user.sub, Option(1)))) {
+                case Success(user) => complete(user)
+                case Failure(ex) => complete(InternalServerError, s"{ error: 'An error occurred: ${ex.getMessage}' }")
+              }
+            }
+            case None => complete(NotFound, s"{ error: 'The user ${userToUpdate} doesn't exist !' }")
+          }
+          case Failure(ex) => complete(InternalServerError, s"{ error: 'An error occurred: ${ex.getMessage}' }")
+        }
+      }
+    }
+  }
+
+
+  val routes: Route = usersGetRoute ~ userGetRoute ~ userPostRoute ~ userPatchRoute ~ userDeleteRoute ~ usersGetBlacklistedRoute ~ usersBlacklistRoute
 
 }
 
