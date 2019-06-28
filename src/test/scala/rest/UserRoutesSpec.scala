aea5a17c459ceee33e55768b8d36edd5ef8b8ad4
Pierre Simon <sim.pierre91@gmail.com>
2019-06-28 10:10:38 +0200

    implem all user routes test

--- src/test/scala/rest/UserRoutesSpec.scala
+++ src/test/scala/rest/UserRoutesSpec.scala
@@ -6,12 +6,12 @@ import persistence.entities.{SimpleUser, User}
 import akka.http.scaladsl.model.StatusCodes._
 import JsonProtocol._
 import SprayJsonSupport._
-import org.scalatest.BeforeAndAfter
+import org.scalatest.BeforeAndAfterAll
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-class UserRoutesSpec extends AbstractRestTest with BeforeAndAfter {
+class UserRoutesSpec extends AbstractRestTest with BeforeAndAfterAll {
 
   import modules.profile.api._
 
@@ -24,12 +24,11 @@ class UserRoutesSpec extends AbstractRestTest with BeforeAndAfter {
     Await.result(modules.db.run(action), Duration.Inf)
   }
 
-  before {
+  override def beforeAll() {
     createSchema()
-    prepareTest()
   }
 
-  after {
+  override def afterAll() {
     dropSchema()
     shutdownDb()
   }
@@ -42,136 +41,6 @@ class UserRoutesSpec extends AbstractRestTest with BeforeAndAfter {
     )
   }
 
-  def prepareTest(): Unit = {
-    "User Routes" should {
-
-      "return all users" in {
-        Get("/users") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-        }
-      }
-
-      "return user not found" in {
-        Get("/users/1") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual NotFound
-        }
-      }
-
-      "create user with id 1" in {
-        Post("/users", SimpleUser("test", 0, 0)) ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual Created
-          responseAs[Option[User]].isEmpty shouldEqual false
-          responseAs[User].pseudo shouldEqual Some("test")
-          responseAs[User].sub shouldEqual Some(0)
-          responseAs[User].blacklist shouldEqual Some(0)
-        }
-      }
-
-      "return user with id 1" in {
-        Get("/users/1") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-          responseAs[Option[User]].isEmpty shouldEqual false
-        }
-      }
-
-      "return empty array of blacklisted users" in {
-        Get("/users/blacklist") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-          responseAs[Seq[User]].isEmpty shouldEqual true
-        }
-      }
-
-      "blacklist users" in {
-        Post("/users/blacklist/1") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-          responseAs[User].blacklist shouldEqual Some(1)
-        }
-      }
-
-      "return array of blacklisted users" in {
-        Get("/users/blacklist") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-          responseAs[Seq[User]].isEmpty shouldEqual false
-        }
-      }
-
-      "unblacklist users" in {
-        Post("/users/unblacklist/1") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-          responseAs[User].blacklist shouldEqual Some(0)
-        }
-      }
-
-      "return empty array of sub users" in {
-        Get("/users/subs") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-          responseAs[Seq[User]].isEmpty shouldEqual true
-        }
-      }
-
-      "sub users" in {
-        Post("/users/subs/1") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-          responseAs[User].sub shouldEqual Some(1)
-        }
-      }
-
-      "return array of sub users" in {
-        Get("/users/subs") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-          responseAs[Seq[User]].isEmpty shouldEqual false
-        }
-      }
-
-      "unsub users" in {
-        Post("/users/unsubs/1") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-          responseAs[User].sub shouldEqual Some(0)
-        }
-      }
-
-      "patch user with id 1" in {
-        Patch("/users/1", SimpleUser("test2", 0, 0)) ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual OK
-          responseAs[Option[User]].isEmpty shouldEqual false
-          responseAs[User].pseudo shouldEqual Some("test2")
-        }
-      }
-
-      "delete user with id 1" in {
-        Delete("/users/1") ~> users.routes ~> check {
-          handled shouldEqual true
-          status shouldEqual (NoContent)
-        }
-      }
-
-      "not handle the invalid json" in {
-        Post("/users", "{\"test\":\"1\"}") ~> users.routes ~> check {
-          handled shouldEqual false
-        }
-      }
-
-      "not handle an empty post" in {
-        Post("/users") ~> users.routes ~> check {
-          handled shouldEqual false
-        }
-      }
-    }
-  }
-
   def dropSchema(): Unit = {
     executeAction(
       DBIO.seq(
@@ -182,6 +51,134 @@ class UserRoutesSpec extends AbstractRestTest with BeforeAndAfter {
 
   def shutdownDb(): Unit = {
     modules.db.close()
+  }
+
+  "User Routes" should {
+
+    "return all users" in {
+      Get("/users") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+      }
+    }
+
+    "return user not found" in {
+      Get("/users/1") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual NotFound
+      }
+    }
+
+    "create user with id 1" in {
+      Post("/users", SimpleUser("test", 0, 0)) ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual Created
+        responseAs[Option[User]].isEmpty shouldEqual false
+        responseAs[User].pseudo shouldEqual Some("test")
+        responseAs[User].sub shouldEqual Some(0)
+        responseAs[User].blacklist shouldEqual Some(0)
+      }
+    }
+
+    "return user with id 1" in {
+      Get("/users/1") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+        responseAs[Option[User]].isEmpty shouldEqual false
+      }
+    }
+
+    "return empty array of blacklisted users" in {
+      Get("/users/blacklist") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+        responseAs[Seq[User]].isEmpty shouldEqual true
+      }
+    }
+
+    "blacklist users" in {
+      Post("/users/blacklist/1") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+        responseAs[User].blacklist shouldEqual Some(1)
+      }
+    }
+
+    "return array of blacklisted users" in {
+      Get("/users/blacklist") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+        responseAs[Seq[User]].isEmpty shouldEqual false
+      }
+    }
+
+    "unblacklist users" in {
+      Post("/users/unblacklist/1") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+        responseAs[User].blacklist shouldEqual Some(0)
+      }
+    }
+
+    "return empty array of sub users" in {
+      Get("/users/subs") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+        responseAs[Seq[User]].isEmpty shouldEqual true
+      }
+    }
+
+    "sub users" in {
+      Post("/users/subs/1") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+        responseAs[User].sub shouldEqual Some(1)
+      }
+    }
+
+    "return array of sub users" in {
+      Get("/users/subs") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+        responseAs[Seq[User]].isEmpty shouldEqual false
+      }
+    }
+
+    "unsub users" in {
+      Post("/users/unsubs/1") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+        responseAs[User].sub shouldEqual Some(0)
+      }
+    }
+
+    "patch user with id 1" in {
+      Patch("/users/1", SimpleUser("test2", 0, 0)) ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual OK
+        responseAs[Option[User]].isEmpty shouldEqual false
+        responseAs[User].pseudo shouldEqual Some("test2")
+      }
+    }
+
+    "delete user with id 1" in {
+      Delete("/users/1") ~> users.routes ~> check {
+        handled shouldEqual true
+        status shouldEqual (NoContent)
+      }
+    }
+
+    "not handle the invalid json" in {
+      Post("/users", "{\"test\":\"1\"}") ~> users.routes ~> check {
+        handled shouldEqual false
+      }
+    }
+
+    "not handle an empty post" in {
+      Post("/users") ~> users.routes ~> check {
+        handled shouldEqual false
+      }
+    }
   }
 
 }
